09113d7762b608ce09547b29fcc62e8d6d8e87d2
Draylar <samuelhimes@gmail.com>
2020-04-27 11:50:32 -0500

    update FAPI for 1.14 branch, version bump to 1.1.0

--- src/main/java/com/github/draylar/ve/VanillaExcavators.java
+++ src/main/java/com/github/draylar/ve/VanillaExcavators.java
@@ -1,11 +1,11 @@
 package com.github.draylar.ve;
 
-import com.github.draylar.ve.config.VanillaExcavatorsConfig;
 import com.github.draylar.ve.common.ExcavatorRegistry;
+import com.github.draylar.ve.config.VanillaExcavatorsConfig;
 import me.sargunvohra.mcmods.autoconfig1.AutoConfig;
 import me.sargunvohra.mcmods.autoconfig1.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.impl.registry.FuelRegistryImpl;
+import net.fabricmc.fabric.impl.content.registry.FuelRegistryImpl;
 
 public class VanillaExcavators implements ModInitializer
 {
