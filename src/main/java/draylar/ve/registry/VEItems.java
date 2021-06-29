dd36e8ab7264dda9bd2996455008a4e620856ffe
Draylar <17728338+Draylar@users.noreply.github.com>
2021-06-28 21:26:47 -0500

    netherite excavator is now fireproof, remove nether things compat (still on 1.14)

--- src/main/java/draylar/ve/registry/VEItems.java
+++ src/main/java/draylar/ve/registry/VEItems.java
@@ -4,7 +4,6 @@ import draylar.magna.item.ExcavatorItem;
 import draylar.ve.VanillaExcavators;
 import draylar.ve.item.FieryExcavatorItem;
 import draylar.ve.material.ExcavatorMaterials;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.item.Item;
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.util.Identifier;
@@ -21,7 +20,7 @@ public class VEItems {
         register(ExcavatorMaterials.IRON, 2, -2.8f, "iron");
         register(ExcavatorMaterials.GOLD, 2, -2.5f, "golden");
         register(ExcavatorMaterials.DIAMOND, 5, -3f, "diamond");
-        register(ExcavatorMaterials.NETHERITE, 6, -3.1f, "netherite");
+        registerFireproof(ExcavatorMaterials.NETHERITE, 6, -3.1f, "netherite");
 
         if (VanillaExcavators.CONFIG.enableExtraMaterials) {
             register(ExcavatorMaterials.EMERALD, 5, -3f, "emerald");
@@ -33,12 +32,6 @@ public class VEItems {
             register(ExcavatorMaterials.ENDER, 6, -3.3f, "ender");
             SLIME = register(ExcavatorMaterials.SLIME, 3, -3f, "slime");
         }
-
-        if (FabricLoader.getInstance().isModLoaded("netherthings")) {
-            register(ExcavatorMaterials.GLOWSTONE, 5, -2.3f, "glowstone");
-            register(ExcavatorMaterials.NETHER, 4, -2.1f, "nether");
-            register(ExcavatorMaterials.VIBRANIUM, 10, -2.9f, "vibranium");
-        }
     }
 
     private static ExcavatorItem register(ToolMaterial material, int attackDamage, float attackSpeed, String excavatorName) {
@@ -47,5 +40,9 @@ public class VEItems {
 
     private static FieryExcavatorItem registerFiery(ToolMaterial material, int attackDamage, float attackSpeed, String excavatorName) {
         return Registry.register(Registry.ITEM, new Identifier("vanillaexcavators", excavatorName + "_excavator"), new FieryExcavatorItem(material, attackDamage, attackSpeed, new Item.Settings().group(VanillaExcavators.GROUP)));
+    }
+
+    private static FieryExcavatorItem registerFireproof(ToolMaterial material, int attackDamage, float attackSpeed, String excavatorName) {
+        return Registry.register(Registry.ITEM, new Identifier("vanillaexcavators", excavatorName + "_excavator"), new FieryExcavatorItem(material, attackDamage, attackSpeed, new Item.Settings().group(VanillaExcavators.GROUP).fireproof()));
     }
 }
